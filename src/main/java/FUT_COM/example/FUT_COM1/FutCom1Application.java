861a3ea5a66bc30e7977627b9ff8c8eccfa68d4b
Diego <di.arroyos@duocuc.cl>
2026-04-21 13:41:36 -0400

    Cambio1

---
+++ src/main/java/FUT_COM/example/FUT_COM1/FutCom1Application.java
@@ -0,0 +1,13 @@
+package FUT_COM.example.FUT_COM1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutCom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutCom1Application.class, args);
+	}
+
+}
---
+++ src/test/java/FUT_COM/example/FUT_COM1/FutCom1ApplicationTests.java
@@ -0,0 +1,13 @@
+package FUT_COM.example.FUT_COM1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutCom1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
